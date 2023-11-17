237cf247187f917dee963af02efaebd866ab3f7b
Purevdorj Batgerel <purvee.hq@gmail.com>
2023-11-16 23:22:23 -0600

    chore: theme add color variables

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,9 +1,52 @@
 import Tokens from '@stadium/token'
 
 export interface Theme {
-  background: string
+  color: {
+    background: {
+      navbar: string
+      elevated: string
+    }
+    text: {
+      default: string
+      subtle: string
+    }
+    border: {
+      default: string
+      subtle: string
+    }
+  }
 }
 
-export const lightTheme: Theme = { background: Tokens.color.neutral_light[100] }
+export const lightTheme: Theme = {
+  color: {
+    background: {
+      navbar: Tokens.color.neutral_light[100],
+      elevated: Tokens.color.neutral_light[300],
+    },
+    text: {
+      default: Tokens.color.neutral_dark[100],
+      subtle: Tokens.color.neutral_dark[200],
+    },
+    border: {
+      default: Tokens.color.neutral_dark[100],
+      subtle: Tokens.color.neutral_dark[300],
+    },
+  },
+}
 
-export const darkTheme: Theme = { background: Tokens.color.neutral_dark[100] }
+export const darkTheme: Theme = {
+  color: {
+    background: {
+      navbar: Tokens.color.neutral_dark[100],
+      elevated: Tokens.color.neutral_dark[300],
+    },
+    text: {
+      default: Tokens.color.neutral_light[100],
+      subtle: Tokens.color.neutral_light[200],
+    },
+    border: {
+      default: Tokens.color.neutral_light[100],
+      subtle: Tokens.color.neutral_light[300],
+    },
+  },
+}
